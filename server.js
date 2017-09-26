db306d0b2f29fc3e2c35a7fc71a647bc7c720116
djay1988 <dilshan.jayasanka88@gmail.com>
2017-09-26 14:47:11 +0530

    Add Simple settings with CORS module

--- server.js
+++ server.js
@@ -1,54 +1,16 @@
-var PORT = 8181;
-var IPADDRESS = '127.0.0.1';
+var express = require("express");
+var app = express();
+var port = 8181;
+var cors = require('cors');
 
-var express = require('express');
-var server;
-var io;
-var app;
 
 app = express();
-// Allow cross origin requests.
-app.use(function (req, res, next) {
-    var origin = '*';
-    
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    next();
-});
-// How we pass our websocket URL to the client.
-app.use('/varSocketURI.js', function (req, res) {
-    var port = PORT;
-    // Modify the URI only if we pass an optional connection port in.
-    var socketURI = port ? ':' + port + '/' : '/';
-    res.set('Content-Type', 'text/javascript');
-    res.send('var socketURI=window.location.hostname+"' + socketURI + '";');
-});
-// The client path is for client specific code.
-app.use('/public', express.static(__dirname + '/public'));
-// The common path is for shared code: used by both client and server.
-app.use('/common', express.static(__dirname + '/common'));
-// The root path should serve the client HTML.
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-});
+app.use(cors());
 
 
+var io = require('socket.io').listen(app.listen(port));
+console.log("Listening on port " + port);
 
-// Our express application functions as our main listener for HTTP requests
-// in this example which is why we don't just invoke listen on the app object.
-server = require('http').createServer(app);
-server.listen(PORT);
-console.log("Server Listen to port : " + PORT);
-
-
-
-
-
-// socket.io augments our existing HTTP server instance.
-io = require('socket.io')(server, {origins:'*:*'});
 
 io.sockets.on('connection', function (socket) {
 
@@ -93,7 +55,7 @@ io.sockets.on('connection', function (socket) {
 //        });
 //    });
     socket.on('g_chat_c', function (data) {
-        console.log("Chat -- " + data.uname +" -"+data.s_id+"-" + " : " + data.text);
+        console.log("Chat -- " + data.uname + " -" + data.s_id + "-" + " : " + data.text);
         io.sockets.emit('g_chat_c', {
             text: data.text,
             s_id: data.s_id,
