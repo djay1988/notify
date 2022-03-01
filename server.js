ae7126c978bab0efdc93f33029ead945186930a0
Dilshan Jayasanka <dilshan.jayasanka88@gmail.com>
2022-03-01 12:56:46 +0530

    CORS Update Test

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app = express();
 app.use(cors());
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
